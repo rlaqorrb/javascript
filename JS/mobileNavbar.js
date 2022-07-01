73396294cea5315415b038871e92ca2c875db2e2
rlaqorrb <80166091+rlaqorrb@users.noreply.github.com>
2022-07-02 00:41:42 +0900

    Meal Search Service update

---
+++ JS/mobileNavbar.js
@@ -0,0 +1,6 @@
+$('#M-slideUp').on('click', function(){
+  $('.M-navbar-container').toggleClass('M-slideUp');
+  $('.M-iconName').toggleClass('M-slideUp');
+  $('.left-bar').toggleClass('M-rotateL');
+  $('.right-bar').toggleClass('M-rotateR');
+})
